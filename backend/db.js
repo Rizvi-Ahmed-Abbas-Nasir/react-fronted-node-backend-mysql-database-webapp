adde7caa2135534f13b6a835e03f3a108d94ba49
FameILY <bhambidsatyam@gmail.com>
2024-09-06 17:12:12 +0530

    Event CRUD added

--- backend/db.js
+++ backend/db.js
@@ -1,44 +1,7 @@
-// const mysql = require("mysql");
-
 const mysql = require("mysql2");
 const { config } = require("dotenv");
 config();
 
-// const connection = mysql.createConnection({
-//   host: process.env.MYSQL_HOST,
-//   port: process.env.MYSQL_PORT,
-//   database: process.env.MYSQL_DATABASE,
-//   user: process.env.MYSQL_USER,
-//   password: process.env.MYSQL_PASSWORD,
-// });
-
-// async function start() {
-//   try {
-//     connection.connect(function (err) {
-//       if (err) {
-//         console.error("error connecting: " + err.stack);
-//         return;
-//       }
-
-//       console.log("connected as id " + connection.threadId);
-//       module.exports = connection;
-//       const app = require("./app");
-//       app.listen(process.env.PORT, () => {
-//         console.log("Server Listening on port " + process.env.PORT);
-//       });
-
-//       connection.query(
-//         "SELECT 1 + 1 AS solution",
-//         function (error, results, fields) {
-//           if (error) throw error;
-//           console.log("The solution is: ", results[0].solution);
-//         }
-//       );
-//     });
-//   } catch (e) {
-//     console.log(e);
-//   }
-// }
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
@@ -50,18 +13,18 @@ const connection = mysql.createPool({
 
 async function start() {
   try {
-    // connecting Database
-    if (connection) {
-      // console.log(connection)
-      console.log("Connected to MYSQL")
-      module.exports = connection;
-      const app = require("./app");
-      app.listen(process.env.PORT, () => {
-        console.log("Server Listening on port " + process.env.PORT);
-      });
-    }
+    // Run a test query to verify the connection
+    await connection.query("SELECT 1");
+
+    console.log("Connected to MYSQL");
+
+    module.exports = connection;
+    const app = require("./app");
+    app.listen(process.env.PORT, () => {
+      console.log("Server Listening on port " + process.env.PORT);
+    });
   } catch (e) {
-    console.log(e);
+    console.error("Error connecting to the database:", e);
   }
 }
 
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,9 +1,22 @@
 const express = require("express")
 const router = express.Router();
-const middleware = require("../middlewares/middleware")
-
+const middleware = require("../middlewares/eventMiddleware")
+const setupController = require("../controllers/setupController")
+const eventController = require("../controllers/eventController")
 router.get('/', (req, res)=> {
     res.status(200).json({message:"Hello world"});
 })
+
+router.post('/setup',  setupController.createTables)
+
+//event handlers
+router.post('/event', eventController.createEvent)
+router.get('/event', eventController.getAllEvents)
+router.put('/event/:id', eventController.updateEvent)
+router.delete('/event/:id', eventController.deleteEvent)
+
+
+
+
 
 module.exports = router
