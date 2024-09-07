35f2ef365a876aeac62a1200e15e011a5934268b
FameILY <bhambidsatyam@gmail.com>
2024-09-07 13:22:31 +0530

    User started

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const eventMiddleware = require("../middlewares/eventMiddleware")
 const setupController = require("../controllers/setupController")
 const eventController = require("../controllers/eventController")
+const userController = require("../controllers/userController")
+
+
 router.get('/', (req, res)=> {
     res.status(200).json({message:"Hello world"});
 })
@@ -14,5 +17,8 @@ router.post('/event', eventMiddleware,eventController.createEvent)
 router.get('/event', eventController.getAllEvents)
 router.put('/event/:id', eventMiddleware, eventController.updateEvent)
 router.delete('/event/:id', eventController.deleteEvent)
+
+//user handlers
+router.post('/userEventReg', userController.registerForEvent )
 
 module.exports = router
